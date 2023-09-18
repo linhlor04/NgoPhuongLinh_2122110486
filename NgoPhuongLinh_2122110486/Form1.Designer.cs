a099a31630c08f324a0f3bb000add2a20fefc7f2
Admin <Admin@D20905>
2023-09-18 13:16:17 +0700

    Add project files.

---
+++ NgoPhuongLinh_2122110486/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace NgoPhuongLinh_2122110486
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnUser = new System.Windows.Forms.Button();
+            this.btnPass = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(110, 52);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(151, 20);
+            this.txtUserName.TabIndex = 0;
+            this.txtUserName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(110, 118);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(151, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // btnUser
+            // 
+            this.btnUser.Location = new System.Drawing.Point(20, 52);
+            this.btnUser.Name = "btnUser";
+            this.btnUser.Size = new System.Drawing.Size(66, 19);
+            this.btnUser.TabIndex = 2;
+            this.btnUser.Text = "UserName";
+            this.btnUser.UseVisualStyleBackColor = true;
+            // 
+            // btnPass
+            // 
+            this.btnPass.Location = new System.Drawing.Point(20, 119);
+            this.btnPass.Name = "btnPass";
+            this.btnPass.Size = new System.Drawing.Size(66, 19);
+            this.btnPass.TabIndex = 3;
+            this.btnPass.Text = "Password";
+            this.btnPass.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(110, 175);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(99, 38);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 313);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnPass);
+            this.Controls.Add(this.btnUser);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnUser;
+        private System.Windows.Forms.Button btnPass;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
